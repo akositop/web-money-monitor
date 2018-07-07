578261c9492b0e071a5d2061498acfa13064966b
kagiwara <gilbert.cuerbo@gmail.com>
2018-07-07 11:26:09 +0800

    add date-picker and test workability

--- src/pages/auth/index.vue
+++ src/pages/auth/index.vue
@@ -2,6 +2,10 @@
     <transition name="zoom" mode="out-in">
         <div class="auth-wrapper">
             <h1>auth</h1>
+            <div class="row">
+              <span>Departure Date：</span>
+              <date-picker :date="startTime" :option="option" :limit="limit"></date-picker>
+            </div>
         </div>
     </transition>
 </template>
